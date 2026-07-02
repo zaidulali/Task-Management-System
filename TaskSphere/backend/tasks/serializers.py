d918148a06a5f50234594192651618e713c0f5ba
zaidulali <zaidulalimohammed@gmail.com>
2026-07-02 16:24:49 +0530

    Implement task CRUD APIs

---
+++ TaskSphere/backend/tasks/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'status', 'created_at', 'updated_at', 'owner')
--- TaskSphere/backend/tasks/views.py
+++ TaskSphere/backend/tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
